3c1cf8d53931d0797f7641946c76b67c2ededf18
DeShawn E <xeno2188@gmail.com>
2023-06-16 14:12:43 -0700

    Added code for redering the game board

---
+++ Coding Projects/Connect Four - C#/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace ConnectFour;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Represent the board.
+            //    1   2   3   4   5   6   7
+            //  +---+---+---+---+---+---+---+
+            //  |   |   |   |   |   |   |   |
+            //  +---+---+---+---+---+---+---+
+            //  |   |   |   |   |   |   |   |
+            //  +---+---+---+---+---+---+---+
+            //  |   |   |   |   |   |   |   |
+            //  +---+---+---+---+---+---+---+
+            //  |   |   |   |   |   |   |   |
+            //  +---+---+---+---+---+---+---+
+            //  |   |   |   |   |   |   |   |
+            //  +---+---+---+---+---+---+---+
+            //  | x |   |   |   |   | o |   |
+            //  +---+---+---+---+---+---+---+
+
+        GameBoard gameBoard = new GameBoard(); 
+        gameBoard.DisplayBoard();
+    }
+}
